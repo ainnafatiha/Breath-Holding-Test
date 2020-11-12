39ddc8541cc76718a49790d99e08c5607bfbf50b
ainnafatiha <ainnafatiha@gmail.com>
2020-11-12 18:02:06 +0800

    new commit - preparation timer

--- bht.js
+++ bht.js
@@ -1,14 +1,38 @@
 // Table of Content
-
 // 1. Codes for reload
 // 2. Codes for circle loader
+// 3. Codes for circle loader
+
 
 // 1. Codes for reload
 function reloadThePage(){
   window.location.reload();
 } 
 
-// 2. Codes for circle loader
+// 2. Codes for preparation timer
+let prepare = document.querySelector(".prepare-timer");
+var seconds = 5;
+var countdownTimer = setInterval(secondPassed, 1500);
+function secondPassed() {
+  setTimeout(() => {
+    var abs_seconds = Math.abs(seconds);
+    var is_negative = seconds > 0;
+    var minutes = Math.round((abs_seconds - 30)/60);
+    var remainingSeconds = abs_seconds % 60; 
+    if (remainingSeconds > 5) 
+    {
+        remainingSeconds = '';
+    }
+    if (minutes < 5) 
+    {
+        minutes = minutes;  
+    }
+    document.querySelector(".prepare-timer").innerHTML = (is_negative ? '-' : '') + minutes + remainingSeconds;
+    seconds--;
+  }, 20500);
+}
+
+// 3. Codes for circle loader
 let text = document.querySelector(".timer");
 let load = document.querySelector(".load");
 
@@ -18,7 +42,7 @@ function startTimer(secs) {
   setTimeout(() => {
     timeInSecs = parseInt(secs);
     ticker = setInterval("tick()", 1000);
-  }, 34000); 
+  }, 39000); 
 }
 
 function tick() {
@@ -33,9 +57,6 @@ function tick() {
   secs %= 60;
   var pretty = (mins < 10 ? "0" : "") + mins + ":" + (secs < 10 ? "0" : "") + secs;
   document.querySelector(".timer").innerHTML = pretty;
+  document.querySelector(".prepare-timer").style.display = 'none';
 }
 startTimer(0 * 60);
- 
-
-
-
