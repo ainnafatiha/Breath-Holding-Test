9806861e48d6daf2ef216bbee2791996d304fcfa
ainnafatiha <ainnafatiha@gmail.com>
2020-12-17 20:53:20 +0800

    new commit
    - timer

--- bht-updated/app.js
+++ bht-updated/app.js
@@ -1,16 +1,19 @@
 /* TABLE OF CONTENT
 
-1. CODES FOR PREPARATION TIMER
+1. CODES FOR TAKE A DEEP BREATH TIMER
 2. CODES FOR HOLD YOUR BREATH TIMER
-3. CODES FOR CSS FUNCTION */
+3. CODES FOR START AND STOP BUTTON FUNCTION 
+
+*/
 
 
-// 1. CODES FOR PREPARATION TIMER
+// 1. CODES FOR TAKE A DEEP BREATH TIMER
 var timeLeft = 3;
 var elem = document.querySelector(".prepare-timer");
 var timerId;
 
 function startCountdown() {
+  document.getElementById("breath-count").innerHTML = "Take a deep breath";
   timerId = setInterval("countdown()", 1200);
 }
 
@@ -37,12 +40,13 @@ function startTimer(secs) {
 }
 
 function tick() {
+  document.getElementById("breath-count").innerHTML = "Hold your breath";
   var secs = timeInSecs;
     if (secs < 60) {
         timeInSecs++;
     } else {
       clearInterval(ticker);
-      startTimer(0 * 60);
+      // startTimer(0 * 60);
     }
   var mins = Math.floor(secs / 60);
   secs %= 60;
@@ -51,18 +55,19 @@ function tick() {
   document.querySelector(".prepare-timer").style.display = 'none';
 }
 
-//3. CODES FOR CSS FUNCTION
+//3. CODES FOR START AND STOP BUTTON FUNCTION
+
+//3.1 start button function
 function startAnimate() {
   startTimer(0 * 60);
-  document.querySelector(".external-circle").style.animationPlayState='running';
-  document.querySelector(".internal-circle").style.animationPlayState='running';
   document.querySelector(".load").style.animationPlayState='running';
-  
+  document.querySelector(".internal-circle").style.animationPlayState='running';
+  document.querySelector(".external-circle").style.animationPlayState='running';
 }
 
+//3.2 stop button function
 function stopAnimate() {
   document.querySelector(".load").style.animationPlayState='paused';
   document.querySelector(".internal-circle").style.animationPlayState='paused';
   document.querySelector(".external-circle").style.animationPlayState='paused';
-
 }
